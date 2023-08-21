addcb8c28be383b1e8b9c0b2611e19c86f352c46
Nicolas <nico.daddabbo7100@gmail.com>
2023-08-21 16:55:52 +0200

    ajout validator custom pour l'age

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,43 +1,75 @@
 import { Component, Injectable, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { RegisterGuard } from 'src/app/shared/interfaces/guard.interface';
 import { UserLogin } from 'src/app/shared/models/user';
+import { checkAge } from 'src/app/utils/function';
 import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 @Injectable()
 export class RegisterComponent implements OnInit, RegisterGuard {
-  constructor(private _authService: AuthService){}
-
-  registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    surname: new FormControl('', [Validators.required]),
-    birthdate: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
-    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)])
-
-  })
-   isDirty(): boolean {
-    return this.registerForm.dirty
+  mailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  registerForm: FormGroup;
+  constructor(private _authService: AuthService, private _fb: FormBuilder) {
+    this.registerForm = this._fb.group({
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      birthdate: ['', [Validators.required, checkAge()]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(this.mailPattern),
+        ],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(this.passwordPattern)],
+      ],
+    });
   }
-ngOnInit(): void {
+
+  isDirty(): boolean {
+    return this.registerForm.dirty;
+  }
+
+  handleSubmit() {
+    console.log(this.registerForm.get('birthdate')?.errors);
+  }
+  ngOnInit(): void {
     let userLogin: UserLogin = {
       email: environment.email,
-      password: environment.password
-    }
+      password: environment.password,
+    };
 
     this._authService.login(userLogin).subscribe({
-      next: (res) => console.log(res)
-      
-    })
-}
+      next: (res) => console.log(res),
+    });
+  }
 
-hasErrorAndTouched(form: FormGroup, input: string, valiator: string): boolean | undefined{
-  return form.get(input)?.hasError(valiator) && (form.get(input)?.touched || form.get(input)?.dirty)
-}
+  hasErrorAndTouched(
+    form: FormGroup,
+    input: string,
+    valiator: string
+  ): boolean | undefined {
+    return (
+      form.get(input)?.hasError(valiator) &&
+      (form.get(input)?.touched || form.get(input)?.dirty)
+    );
+  }
 }
---
+++ src/app/utils/function.ts
@@ -0,0 +1,31 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function checkAge(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      let actualDate = new Date();
+      let actualYear = actualDate.getFullYear();
+      let actualMonth = actualDate.getMonth();
+      let actualDay = actualDate.getDate();
+
+      let year: Date = new Date(control.value);
+      let userYearBirthdate = year.getFullYear();
+      let userMonthBirthdate = year.getMonth();
+      let userDayBirthdate = year.getDate();
+
+      if (actualYear - userYearBirthdate === 18) {
+        if (actualMonth === userMonthBirthdate) {
+          
+          if (userDayBirthdate > actualDay) {
+            console.log('dd');
+            return { notMajor: true };
+          }
+        }
+      } else if (actualYear - userYearBirthdate < 18) {
+        return { notMajor: true };
+      } else {
+        return null;
+      }
+
+      return null
+    };
+  }
