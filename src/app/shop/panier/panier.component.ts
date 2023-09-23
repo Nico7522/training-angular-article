69a7d339e566b90c3814f66ea2342980a5cdfec8
Nicolas <nico.daddabbo7100@gmail.com>
2023-09-23 12:09:41 +0200

    ajout image

--- src/app/shop/panier/panier.component.ts
+++ src/app/shop/panier/panier.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { ShopService } from 'src/app/services/shop.service';
 import { CommandUser } from 'src/app/shared/models/command';
 import { Product, ProductWithQuantity } from 'src/app/shared/models/product';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-panier',
@@ -10,6 +11,8 @@ import { Product, ProductWithQuantity } from 'src/app/shared/models/product';
   styleUrls: ['./panier.component.css']
 })
 export class PanierComponent implements OnInit, OnDestroy {
+  imgUrl = environment.apiUrlImg
+
   constructor(private _shopService: ShopService){}
   panier: ProductWithQuantity[] | undefined ;
   totalPrice: number = 0;
